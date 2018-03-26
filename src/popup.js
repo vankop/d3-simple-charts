0803e3573bdde9bab10073b601ee99381e4371b4
ivan.kopeykin <ivan.kopeykin@gmail.com>
2018-03-26 12:08:44 +0300

    fix(popup): reworked mouse leave event

--- src/popup.js
+++ src/popup.js
@@ -20,77 +20,14 @@ const baseWidth = 80;
 export default function popup(node, serieName, serieInfo, coord, color, onMouseLeave) {
     const popup = node
         .append('g')
+            .style('cursor', 'default')
             .attr('transform', `translate(${coord})`);
-
-    const rect = popup
-        .append('rect')
-        .attr('x', 0)
-        .attr('y', 0)
-        .attr('fill', '#fff')
-        .attr('stroke', '#000')
-        .attr('height', legendInfoRadius * 2 + extraHeight + border)
-        .attr('width', baseWidth + max([serieName.length, serieInfo.length]));
-
-    const circle = popup
-        .append('circle')
-        .attr('fill', color)
-        .attr('cx', legendInfoRadius + paddingInRect)
-        .attr('cy', legendInfoRadius + paddingInRect)
-        .attr('r', legendInfoRadius);
-
-    const name = popup
-        .append('text')
-        .attr('font-family', 'sans-serif')
-        .attr('font-size', '14px')
-        .attr('x', 2 * legendInfoRadius + paddingInRect + legendCircleTextPadding)
-        .attr('y', 2 * legendInfoRadius + paddingInRect)
-        .text(serieName);
-
-    const info = popup
-        .append('text')
-        .attr('font-family', 'sans-serif')
-        .attr('font-size', '14px')
-        .attr('x', paddingInRect)
-        .attr('y', 2 * legendInfoRadius + extraHeight + paddingInRect - 10)
-        .text(serieInfo);
 
     const isPopupContent = (domNode) => {
         let result = false;
 
-        rect.each(function () {
-            if (this === domNode) {
-                result = true;
-            }
-        });
-
-        if (result) {
-            return result;
-        }
-
-        name.each(function () {
-            if (this === domNode) {
-                result = true;
-            }
-        });
-
-        if (result) {
-            return result;
-        }
-
-        info.each(function () {
-            if (this === domNode) {
-                result = true;
-            }
-        });
-
-        if (result) {
-            return result;
-        }
-
-        circle.each(function () {
-            if (this === domNode) {
-                result = true;
-            }
+        popup.each(function () {
+            result = this.contains(domNode)
         });
 
         return result;
@@ -102,10 +39,41 @@ export default function popup(node, serieName, serieInfo, coord, color, onMouseL
         }
     };
 
-    rect.on('mouseleave', handleMouseLeave);
-    name.on('mouseleave', handleMouseLeave);
-    info.on('mouseleave', handleMouseLeave);
-    circle.on('mouseleave', handleMouseLeave);
+    const rect = popup
+        .append('rect')
+        .on('mouseleave', handleMouseLeave)
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('fill', '#fff')
+        .attr('stroke', '#000')
+        .attr('height', legendInfoRadius * 2 + extraHeight + border)
+        .attr('width', baseWidth + max([serieName.length, serieInfo.length]));
+
+    const circle = popup
+        .append('circle')
+        .on('mouseleave', handleMouseLeave)
+        .attr('fill', color)
+        .attr('cx', legendInfoRadius + paddingInRect)
+        .attr('cy', legendInfoRadius + paddingInRect)
+        .attr('r', legendInfoRadius);
+
+    const name = popup
+        .append('text')
+        .on('mouseleave', handleMouseLeave)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '14px')
+        .attr('x', 2 * legendInfoRadius + paddingInRect + legendCircleTextPadding)
+        .attr('y', 2 * legendInfoRadius + paddingInRect)
+        .text(serieName);
+
+    const info = popup
+        .append('text')
+        .on('mouseleave', handleMouseLeave)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '14px')
+        .attr('x', paddingInRect)
+        .attr('y', 2 * legendInfoRadius + extraHeight + paddingInRect - 10)
+        .text(serieInfo);
 
     return {
         isEventTarget: isPopupContent,
